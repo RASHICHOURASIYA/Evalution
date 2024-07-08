f266ef77033a1868175b6bfcb366cfd400d17515
rashi <rashichourasiya9026@gmail.com>
2024-07-08 18:03:50 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,37 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
+const orderRouter = require('./src/routes/OrderRouter');
+const reviewRouter = require('./src/routes/reviewRouter');
+const connectionDB = require('./src/config/db');
+const sequelize = require('./src/config/db');
+
 
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port =  8081;
+
+app.use(express.json());
+app.use(orderRouter);
+app.use(reviewRouter);
+
 
 app.get("/", (req, res)=>{
-    console.log(" this is home route");
+    res.send(" this is home route");
 });
 
 
+
+
+mongoose.connect("mongodb+srv://bookstore:bookstore@cluster0.7szyrkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => console.log('Connected with mongoose'));
+
+sequelize.sync().then(()=>{
 app.listen(port, ()=>{
+//connectionDB.connect((err)=>{
+//    if(err) console.log(err);
+//    else console.log("we are connected with mysql");
+//})
     console.log(`Server running on port no ${port}`);
-})
+});
+});
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,2 +1,30 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.MYSQL_URI);
+//const mysql = require('mysql2');
+//
+//const connectionDB = mysql.createConnection({
+//    host: 'localhost',
+//    user: 'root',
+//    database: 'evauser',
+//    password:"rakhi9026"
+//  });
+//
+//
+//  module.exports = connectionDB;
+
+
+const{Sequelize} = require('sequelize');
+const sequelize = new Sequelize(process.env.MYSQL_URl,{
+    dialect:"mysql",
+    logging: console.log,
+    define:{
+        timestamps : false
+    }
+});
+
+sequelize.authenticate().then(()=>{
+    console.log('Connected successfully');
+})
+.catch(err=>{
+    console.error(err);
+});
+
+module.exports = sequelize;
