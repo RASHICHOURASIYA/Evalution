aab3219f65985f88fc3e734892ed1ce6651ddb9a
rashi <rashichourasiya9026@gmail.com>
2024-07-11 17:33:15 +0530

    node

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
-const orderRouter = require('./src/routes/OrderRouter');
-const reviewRouter = require('./src/routes/ReviewRouter');
-const sequelize = require('./src/config/db');
-const customerRoute = require('./src/routes/CustomerRouter');
 const cors = require('cors');
+const sequelize = require('./src/config/db');
+const orderRouter = require('./src/routes/OrderRouter');
+const customerRoute = require('./src/routes/CustomerRouter');
+const reviewRouter = require('./src/routes/ReviewRouter');
 
 const app = express();
-const port =  8083;
+const port = 8083;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use(orderRouter);
-app.use(reviewRouter);
+
+// Routes
+app.use('/api/orders', orderRouter);
 app.use('/api/customers', customerRoute);
+app.use('/api/reviews', reviewRouter);
 
 app.get("/", (req, res) => {
   res.send("This is the home route");
 });
 
-
-
+// MongoDB Connection
 mongoose.connect("mongodb+srv://bookstore:bookstore@cluster0.7szyrkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => console.log('Connected with mongoose'))
-.catch((err) => console.log('Failed to connect with mongoose', err));
+  .then(() => console.log('Connected to MongoDB with Mongoose'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
 
-sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-});
-
-
-//sequelize.sync().then(()=>{
-//app.listen(port, ()=>{
-//connectionDB.connect((err)=>{
-//    if(err) console.log(err);
-//    else console.log("we are connected with mysql");
-//})
-   // console.log(`Server running on port no ${port}`);
-//});
-//});
+// Sequelize (SQL) Connection
+sequelize.sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(err => console.log('Failed to sync with Sequelize', err));
